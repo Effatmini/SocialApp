2bedb34f5842938e611ebdd2a10903a3dd504474
Effat Mini <effatmini@gmail.com>
2026-04-08 19:46:18 +0200

    update file

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,7 @@ import connectDB from "./config/db";
 // import { Admin } from "./models/admin";
 import express from "express";
 import userRoutes from "./routes/userRoutes";
-// const user1 = new User(
-//   1,
-//   "Mustafa",
-//   "mustafa@gmail.com",
-//   "123456",
-//   "01000000000",
-//   25
-// );
 
-// user1.displayInfo();
-
-// console.log("------------");
-
-// const admin1 = new Admin(
-//   2,
-//   "Ahmed",
-//   "admin@gmail.com",
-//   "admin123",
-//   "01111111111",
-//   30
-// );
-
-// admin1.displayInfo();
-// admin1.manageUsers();
 const app = express();
 
 app.use(express.json());
